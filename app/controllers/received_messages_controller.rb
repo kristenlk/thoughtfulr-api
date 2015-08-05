1e08750936ed472615a0fd7ea93c2f50ea625b9a
kristenlk <klkehlenbeck@gmail.com>
2015-08-05 09:13:12 -0400

    Twilio texts sending, row added to ReceivedMessages table. Adding additional conditions next.

--- app/controllers/received_messages_controller.rb
+++ app/controllers/received_messages_controller.rb
@@ -42,13 +42,20 @@ class ReceivedMessagesController < ApplicationController
 
 
   def create #send_message
-    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID_PRODUCTION'], ENV['TWILIO_AUTH_TOKEN_PRODUCTION']
-    @twilio_number = ENV['TWILIO_NUMBER']
+    # @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID_PRODUCTION'], ENV['TWILIO_AUTH_TOKEN_PRODUCTION']
+    # @twilio_number = ENV['TWILIO_NUMBER']
     body = Message.find(5).body
-    @message = @client.account.messages.create({ :to => '+12039066039',
-                                                 :from => @twilio_number,
-                                                 :body => body })
-    ReceivedMessage.new(user_id: @current_user.user_id, message_id: body)
+    # @message = @client.account.messages.create({ :to => '+12039066039',
+    #                                              :from => @twilio_number,
+    #                                              :body => body })
+    # byebug
+    rcvd_message = ReceivedMessage.new(user_id: @current_user.id, message_id: Message.find(5).id)
+    if rcvd_message.save
+      render json: rcvd_message
+      # increment sent_message count in Profiles table by 1
+    else
+      render json: rcvd_message.errors, status: :unprocessable_entity
+    end
   end
 
 
